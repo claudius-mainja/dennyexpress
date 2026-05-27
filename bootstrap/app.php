b0405d1aa5c96ab96f49d5236beca7085300c7a6
Claudisu Mainja <claudius.mainja@gmail.com>
2026-05-27 13:03:40 +0200

    Exclude migrate/seed routes from CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '__migrate',
+            '__seed',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Throwable $e) {
