db647db90fc0426a559dba0d66dee58c0cfa4768
Claudisu Mainja <claudius.mainja@gmail.com>
2026-05-27 13:05:35 +0200

    Use migrate:fresh to handle existing tables

--- routes/migrate.php
+++ routes/migrate.php
@@ -8,11 +8,19 @@ Route::post('/__migrate', function (Request $request) {
         return response()->json(['error' => 'Unauthorized'], 401);
     }
 
-    $result = Artisan::call('migrate', ['--force' => true]);
-    return response()->json([
-        'exit_code' => $result,
-        'output' => Artisan::output(),
-    ]);
+    try {
+        $result = Artisan::call('migrate:fresh', ['--force' => true]);
+        return response()->json([
+            'exit_code' => $result,
+            'output' => Artisan::output(),
+        ]);
+    } catch (\Throwable $e) {
+        return response()->json([
+            'exit_code' => 1,
+            'output' => Artisan::output(),
+            'error' => $e->getMessage(),
+        ]);
+    }
 });
 
 Route::post('/__seed', function (Request $request) {
